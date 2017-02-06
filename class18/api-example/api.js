2319ceb83bb485f29ca638eba7cf8c63547838b7
heidi schmidt <heidischmidt91@gmail.com>
2017-02-06 18:24:24 -0500

    beginning of class 2_6

---
+++ class18/api-example/api.js
@@ -0,0 +1,17 @@
+var title = "";
+
+$('#submit').click(function(e){
+	title = $('#title').val();
+	getMovie(title);
+    e.preventDefault();
+});
+
+function getMovie(movieName)
+{
+    $.getJSON("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json", function(json) {
+         console.log(json.Director); 
+    });
+}
+
+
+
